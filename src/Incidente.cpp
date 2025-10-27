541979572c8550bc30d49f300879542a3825015f
CodeNilo <120208172+CodeNilo@users.noreply.github.com>
2025-10-27 18:18:40 -0500

    Quitar comentarios en Incidente.cpp
    Removed commented-out block of instructions and notes.

--- src/Incidente.cpp
+++ src/Incidente.cpp
@@ -1,15 +1,3 @@
-/* 
-
-(Piero)
-
-- Implementar los métodos declarados en Incidente.h
-- Constructor que inicializa los atributos.
-- Métodos getter.
-- Método mostrarInfo() que imprime todos los datos del incidente.
-- Sobrecarga del operador < para comparar prioridades.
-
-*/ 
-
 #include "Incidente.h"
 #include <iostream>
 
