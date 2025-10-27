20f36a73f37fd044ac42afde5c8fd15fb27b5cc2
CodeNilo <120208172+CodeNilo@users.noreply.github.com>
2025-10-27 21:49:10 +0000

    - Estructuracion inciial del proyecto
    - Implementacion del LeftistHeap y sus metodos como header.

---
+++ include/LeftistTree.h
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+
+struct Nodo {
+    int dato;
+    int npl;
+
+    Nodo* izq;
+    Nodo* der;
+
+    Nodo(int dx) : dato(dx), npl(0), izq(nullptr), der(nullptr) {} 
+
+};
+
+static Nodo* merge(Nodo* heap, Nodo* nuevoNodo){
+    if(!heap) return nuevoNodo;
+    if(!nuevoNodo) return heap;
+    
+    if(heap->dato > nuevoNodo->dato)
+        swap(heap, nuevoNodo);
+    
+    heap->der = merge(heap->der, nuevoNodo);
+    
+    int nplD = heap->der ? heap->der->npl : -1;
+    int nplI = heap->izq ? heap->izq->npl : -1;
+
+    if(nplI < nplD){
+        swap(heap->der, heap->izq);
+    }
+    
+    heap->npl = (heap->der ? heap->der->npl : -1) + 1;
+    
+    return heap;
+
+}
+
+static Nodo* insertar(Nodo* heap, int dx){
+    Nodo* nuevoNodo = new Nodo(dx);
+    return merge(heap, nuevoNodo);
+}
+
+static Nodo* eliminarMin(Nodo* head){
+    if(!head) return nullptr;
+
+    Nodo *nuevoNodo = merge(head->izq, head->der);
+    delete head;
+    return nuevoNodo;
+}
+
+static void printHeap(Nodo* heap, int padre){
+    if(!heap) return;
+
+    cout<<endl<<"Valor actual: "<<heap->dato;
+    cout<<", Padre" << padre;
+    cout<<", NPL: "<<heap->npl;
+
+    cout<<endl<<"Hijo Izquierdo: ";
+    printHeap(heap->izq, heap->dato);
+    cout<<endl<<"Hijo Derecho: ";
+    printHeap(heap->der, heap->dato);
+}
